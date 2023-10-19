dc9a42a5b46b58f67c3b6eb06ee3947e636389c8
Luis <posadaluis451@gmail.com>
2023-10-18 23:14:18 -0500

    Login, sign up y logout

---
+++ src/app/components/sign-up/sign-up.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent {
+  constructor(private authService: AuthService) {
+
+  }
+  signUp(email: string, password: string) {
+    this.authService.signUpWithEmailAndPassword(email, password);
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'shop-38283',
+    appId: '1:689214142624:web:b49503c152426786e8b1c5',
+    storageBucket: 'shop-38283.appspot.com',
+    apiKey: 'AIzaSyAjhH2J3XmKEXGyo0pQvin8ow6scvBLkEQ',
+    authDomain: 'shop-38283.firebaseapp.com',
+    messagingSenderId: '689214142624',
+  },
+};
